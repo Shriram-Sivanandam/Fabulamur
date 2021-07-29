f5b6bc2fc055aa23616f2bddbde73b133a3c3c0d
Shriram-Sivanandam <ssshriram4@gmail.com>
2021-07-29 17:26:22 +0530

    almost ready

---
+++ src/components/InfoBar/InfoBar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Link } from "react-chrome-extension-router";
+
+import home from "../../icons/fi_home.png";
+// import users from "../../icons/people.png";
+
+import Choose from "../Choose/Choose";
+
+import "./InfoBar.css";
+
+const InfoBar = ({ room }) => (
+	<div className="infoBar">
+		<div className="leftInnerContainer">
+			<h3>Room: {room}</h3>
+		</div>
+		<div className="rightInnerContainer">
+			{/* <div onClick={() => setShowModal(!showModal)}>
+        <img className="usersIconDiv" src={users} alt="users" />
+      </div> */}
+			<Link component={Choose}>
+				<img src={home} alt="home" />
+			</Link>
+		</div>
+	</div>
+);
+
+export default InfoBar;
